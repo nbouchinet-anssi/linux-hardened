0c2ae48ad418f78859d5375430ae144e3bb1f540
Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
2019-05-07 11:46:21 +0200

    mm: Fix extra_latent_entropy
    Commit a9cd410a3d29 ("mm/page_alloc.c: memory hotplug: free pages as
    higher order") changed `static void __init __free_pages_boot_core()`
    into `void __free_pages_core()`, causing the following section mismatch
    warning at compile time:
    
        WARNING: vmlinux.o(.text+0x180fe4): Section mismatch in reference from the function __free_pages_core() to the variable .meminit.data:extra_latent_entropy
        The function __free_pages_core() references the variable __meminitdata extra_latent_entropy.
        This is often because __free_pages_core lacks a __meminitdata annotation or the annotation of extra_latent_entropy is wrong.
    
    This commit is an attempt at fixing this issue. I'm not sure it's OK as
    we are accessing pages that are still managed by the memblock allocator.
    The prefetching part is not an issue as it only affects struct pages.
    
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    [levente@leventepolyak.net: most of core MM initialization moved to mm/mm_init.c]
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    [nicolas.bouchinet@ssi.gouv.fr: MAX_ORDER has been renamed to MAX_PAGE_ORDER (see 5e0a760b44417f7ca)]
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -1998,6 +1998,7 @@ static void __init deferred_free_pages(unsigned long pfn,
 	if (nr_pages == MAX_ORDER_NR_PAGES && IS_MAX_ORDER_ALIGNED(pfn)) {
 		for (i = 0; i < nr_pages; i += pageblock_nr_pages)
 			set_pageblock_migratetype(page + i, MIGRATE_MOVABLE);
+		__gather_extra_latent_entropy(page, 1 << MAX_PAGE_ORDER);
 		__free_pages_core(page, MAX_PAGE_ORDER, MEMINIT_EARLY);
 		return;
 	}
@@ -2008,6 +2009,7 @@ static void __init deferred_free_pages(unsigned long pfn,
 	for (i = 0; i < nr_pages; i++, page++, pfn++) {
 		if (pageblock_aligned(pfn))
 			set_pageblock_migratetype(page, MIGRATE_MOVABLE);
+		__gather_extra_latent_entropy(page, 1);
 		__free_pages_core(page, 0, MEMINIT_EARLY);
 	}
 }
@@ -2560,6 +2562,7 @@ void __init memblock_free_pages(struct page *page, unsigned long pfn,
 
 	/* pages were reserved and not allocated */
 	clear_page_tag_ref(page);
+	__gather_extra_latent_entropy(page, 1 << order);
 	__free_pages_core(page, order, MEMINIT_EARLY);
 }
 
