a64af940a7a4d44c660bc771ba958d4dd9e2949e
Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>
2023-12-08 11:53:31 +0100

    io_uring: set io_uring_disabled sysctl to 1 by default
    This forces processes to have `CAP_SYS_ADMIN` in order to use io_uring or
    to be in the io_uring_group.
    
    The patch alter the sysctl value range in order that once set to "2" it
    can't be lowered again.
    
    The io_uring_group sysctl option is set to -1 by default, user should
    define a proper group and set the sysctl properly if they want it configured.
    
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -140,6 +140,8 @@ struct io_defer_entry {
 /* Forced wake up if there is a waiter regardless of ->cq_wait_nr */
 #define IO_CQ_WAKE_FORCE	(IO_CQ_WAKE_INIT >> 1)
 
+#define IO_URING_MAX 2
+
 static bool io_uring_try_cancel_requests(struct io_ring_ctx *ctx,
 					 struct io_uring_task *tctx,
 					 bool cancel_all,
@@ -153,8 +155,23 @@ static __read_mostly DEFINE_STATIC_KEY_DEFERRED_FALSE(io_key_has_sqarray, HZ);
 struct kmem_cache *req_cachep;
 static struct workqueue_struct *iou_wq __ro_after_init;
 
-static int __read_mostly sysctl_io_uring_disabled;
+static int __read_mostly sysctl_io_uring_disabled = 1;
 static int __read_mostly sysctl_io_uring_group = -1;
+
+static int io_uring_dointvec_minmax(const struct ctl_table *table, int write,
+                               void *buffer, size_t *lenp, loff_t *ppos)
+{
+       struct ctl_table table_copy;
+
+       /* Lock the max value if it ever gets set. */
+       table_copy = *table;
+       if (*(int *)table_copy.data == *(int *)table_copy.extra2)
+               table_copy.extra1 = table_copy.extra2;
+
+       return proc_dointvec_minmax(&table_copy, write, buffer, lenp, ppos);
+}
+
+static int io_uring_max = IO_URING_MAX;
 
 #ifdef CONFIG_SYSCTL
 static const struct ctl_table kernel_io_uring_disabled_table[] = {
@@ -163,9 +180,10 @@ static const struct ctl_table kernel_io_uring_disabled_table[] = {
 		.data		= &sysctl_io_uring_disabled,
 		.maxlen		= sizeof(sysctl_io_uring_disabled),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		/* Do not allow do go back if io_uring_disabled is set to 2 */
+		.proc_handler	= io_uring_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_TWO,
+		.extra2		= &io_uring_max,
 	},
 	{
 		.procname	= "io_uring_group",
