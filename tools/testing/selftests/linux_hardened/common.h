b562bd0ee17511995f2f6b8a723f1f74bcd04f58
Nicolas Bouchinet <Nicolas Bouchinet>
2024-10-16 09:27:28 +0200

    selftests: Add sysctl tests
    Signed-off-by: Nicolas Bouchinet <Nicolas Bouchinet>

--- tools/testing/selftests/linux_hardened/common.h
+++ tools/testing/selftests/linux_hardened/common.h
@@ -6,12 +6,18 @@
 #include "../kselftest_harness.h"
 
 #include <errno.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <sys/capability.h>
 #include <linux/sched.h>
 #include <sched.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <asm/termbits.h>
+#include <linux/limits.h>
 
 #define STACK_SIZE (1024 * 1024)
 
@@ -19,18 +25,71 @@ static int open_or_die(const char *filename, int flags)
 {
 	int fd = open(filename, flags);
 
-	if (fd < 0)
-		ksft_exit_fail_msg("Failed to open '%s'; "
-			"check prerequisites are available\n", filename);
+	if (fd < 0) {
+		ksft_exit_fail_perror(filename);
+	}
 	return fd;
 }
 
 static void check_user_id(int needed_uid)
 {
-	int actual_uid = getuid();
+	int actual_uid = geteuid();
 	if (actual_uid != needed_uid) {
-		ksft_exit_fail_msg("invalid uid, got %i instead of %i", actual_uid, needed_uid);
+		ksft_exit_fail_msg("invalid euid, got %i instead of %i", actual_uid, needed_uid);
 	}
+}
+
+static pid_t clone3(struct clone_args *args, size_t size)
+{
+	fflush(stdout);
+	fflush(stderr);
+	return syscall(__NR_clone3, args, size);
+}
+
+static int drop_cap_sys_admin()
+{
+	cap_t caps = cap_get_proc();
+	cap_value_t cap_list[CAP_LAST_CAP + 1];
+
+	if (caps == NULL)
+		return -1;
+
+	cap_list[0] = CAP_SYS_ADMIN;
+	if (cap_set_flag(caps, CAP_EFFECTIVE, 1, cap_list, CAP_CLEAR) < 0) {
+		cap_free(caps);
+		return -1;
+	}
+
+	if (cap_set_proc(caps) < 0) {
+		cap_free(caps);
+		return -1;
+	}
+
+	cap_free(caps);
+	return 0;
+}
+
+static int set_cap_sys_admin()
+{
+	cap_t caps = cap_get_proc();
+	cap_value_t cap_list[CAP_LAST_CAP + 1];
+
+	if (caps == NULL)
+		return -1;
+
+	cap_list[0] = CAP_SYS_ADMIN;
+	if (cap_set_flag(caps, CAP_EFFECTIVE, 1, cap_list, CAP_SET) < 0) {
+		cap_free(caps);
+		return -1;
+	}
+
+	if (cap_set_proc(caps) < 0) {
+		cap_free(caps);
+		return -1;
+	}
+
+	cap_free(caps);
+	return 0;
 }
 
 static int dummy(void *arg){
